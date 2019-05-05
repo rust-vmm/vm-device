11f3d39932c0f6467b27bc67ccba4451e3182383
Jing Liu <jing2.liu@linux.intel.com>
2019-05-05 19:31:42 +0800

    vm-device: Add basic files
    Add and correct basic support files for the vm-device crate including:
    - files generated by Cargo
    - license files
    
    Signed-off-by: Jing Liu <jing2.liu@linux.intel.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,2 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
+// Copyright © 2019 Intel Corporation. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0 OR BSD-3-Clause
